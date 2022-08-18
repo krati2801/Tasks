133fe22b2d97a369c3db3296becec9d3dd5c0824
Desktop <Desktop@gmail.com>
2022-08-18 12:35:28 +0530

    Deleted Employee Management System Task

--- Employee Management System/Backend/Configs/globals.js
+++
@@ -1,7 +0,0 @@
-global.chalk = require('chalk'); // FOR COLORED CONSOLES
-
-global.STATUS_CODES = require('./constants').STATUS_CODES
-
-//DB CONNECTION
-global.sequelize = require('./database').sequelize
-global.Op = sequelize.Op
--- Employee Management System/Backend/Middleware/Validators/Employee.Validator.js
+++
@@ -1,8 +0,0 @@
-const { header, body } = require("express-validator")
-
-exports.addEmployee = [
-    body("firstName", "please enter firstName!!").trim().notEmpty(),
-    body("lastName", "please enter lastName!!").trim().notEmpty(),
-    body("email", "Email is required").trim().notEmpty().isEmail(),
-    body("phone", "Mobile Number is required").trim().notEmpty().isNumeric()
-]
